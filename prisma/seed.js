4b36c3ea1081205530ca5741d3121296e7db9e65
sakeensim <sakeensim@gmail.com>
2025-02-18 13:33:26 +0700

    update img in seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -5,30 +5,22 @@ const hashedPassword = bcrypt.hashSync('123456',10)
 
 const userData = [
     {
-        firstName: 'Andy',
-        lastName: 'Codecamp',
-        email: 'andy@ggg.mail',
-        password: hashedPassword
+     firstName: 'Andy', lastName: 'Codecamp', password: hashedPassword, email: 'andy@ggg.mail',
+     profileImage: 'https://www.svgrepo.com/show/420364/avatar-male-man.svg'
     },
     {
-        firstName: 'Bobby',
-        lastName: 'Codecamp',
-        email: 'bobby@ggg.mail',
-        password: hashedPassword
+     firstName: 'Bobby', lastName: 'Codecamp', password: hashedPassword, email: 'bobby@ggg.mail',
+     profileImage: 'https://www.svgrepo.com/show/420319/actor-chaplin-comedy.svg'
     },
     {
-        firstName: 'Candy',
-        lastName: 'Codecamp',
-        email: '1111111111',
-        password: hashedPassword
+     firstName: 'Candy', lastName: 'Codecamp', password: hashedPassword, mobile: '1111111111',
+     profileImage: 'https://www.svgrepo.com/show/420327/avatar-child-girl.svg'
     },
     {
-        firstName: 'Danny',
-        lastName: 'Codecamp',
-        email: '2222222222',
-        password: hashedPassword
+     firstName: 'Danny', lastName: 'Codecamp', password: hashedPassword, mobile: '2222222222',
+     profileImage: 'https://www.svgrepo.com/show/420314/builder-helmet-worker.svg'
     },
-]
+   ]
 console.log('DB seed...')
 async function seedDB(){
     await prisma.user.createMany({data: userData})
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 require('dotenv').config()
 const express = require("express")
+const cors =require('cors')
+const helmet = require('helmet')
+const morgan = require('morgan')
 const notFound = require('./middlewares/notFound')
 const errorMiddleware = require('./middlewares/errorMiddleware')
 const authRoute = require('./routes/auth-route')
+
 
 
 const app = express()
@@ -12,7 +16,17 @@ const app = express()
 // app.use("*",()=>{}) รับได้ทุก method
 // app.get("*",()=>{}) รับแค่ Get
 
+    // app.use(cors({
+    //     origin:'http://localhost:5173',
+    // }))
+
+    app.use(morgan('dev'))
+    app.use(helmet())
+    app.use(cors())
+
+    
     app.use(express.json())
+
 
 
     app.use("/auth",authRoute)
